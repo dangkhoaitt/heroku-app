0ccb71255bcf94cdd96821b39e077a1852272c7a
KHOA_VO <dangkhoaitt@gmail.com>
2021-11-28 16:38:52 +0700

    fix path

--- app.js
+++ app.js
@@ -2,25 +2,12 @@ const express = require("express");
 const path = require("path");
 
 const app = express();
-app.use(express.static(path.join(__dirname, "build")));
-
-// catch 404 and forward to error handler
-app.use(function (req, res, next) {
-  next(createError(404));
-});
-
-// error handler
-app.use(function (err, req, res, next) {
-  // set locals, only providing error in development
-  res.locals.message = err.message;
-  res.locals.error = req.app.get("env") === "development" ? err : {};
-
-  // render the error page
-  res.status(err.status || 500);
-  res.render("error");
-});
-app.get("/", (req, res) => res.send("build/index.html"));
-app.listen(5000, () => {
-  console.log("app is listen port >>", 3000);
-});
+app
+  .use(express.static(path.join(__dirname, "build")))
+  .get('/*', (req, res) => {
+    res.sendFile(path.join(__dirname, 'build', 'index.html'));
+  })
+  .listen(3000, () => {
+    console.log("app is listen port >>", 3000);
+  });
 
